0356dd0fbc12f1014c275264206557b52cdb7190
Mwni <manuel@tigmar.de>
2022-09-22 04:08:27 +0200

    add distribution manifest

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+	name='diffusionkit',
+	version='0.7',
+	packages=[
+		'diffusionkit',
+		'diffusionkit.configs',
+		'diffusionkit.models',
+		'diffusionkit.models.diffusion',
+		'diffusionkit.modules',
+		'diffusionkit.modules.diffusion',
+		'diffusionkit.modules.image_degradation',
+		'diffusionkit.modules.losses'
+	],
+	install_requires=[
+		'diffusers',
+		'einops',
+		'kornia',
+		'omegaconf',
+		'transformers',
+		'pytorch-lightning',
+		'clip @ git+https://github.com/openai/CLIP#egg=clip',
+		'taming-transformers @ git+https://github.com/illeatmyhat/taming-transformers#egg=taming-transformers',
+		'k_diffusion @ git+https://github.com/hlky/k-diffusion-sd#egg=k_diffusion'
+	],
+	package_data={
+		'diffusionkit.configs': ['*.yaml']
+	}
+)
