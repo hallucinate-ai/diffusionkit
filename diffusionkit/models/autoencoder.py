5702d8f0ddf6eb5433732b16b66d491a5624ae7b
Mwni <manuel@tigmar.de>
2022-12-25 22:27:20 +0100

    upgrade to new v2 compatible code

--- diffusionkit/models/autoencoder.py
+++ diffusionkit/models/autoencoder.py
@@ -7,42 +7,29 @@ from ..modules.utils import instantiate_from_config
 
 class AutoencoderKL(torch.nn.Module):
 	def __init__(self,
-				 ddconfig,
-				 lossconfig,
-				 embed_dim,
-				 ckpt_path=None,
-				 ignore_keys=[],
-				 image_key="image",
-				 colorize_nlabels=None,
-				 monitor=None,
-				 ):
+		ddconfig,
+		embed_dim,
+		image_key="image",
+		colorize_nlabels=None,
+	):
 		super().__init__()
+
 		self.image_key = image_key
 		self.encoder = Encoder(**ddconfig)
 		self.decoder = Decoder(**ddconfig)
-		self.loss = instantiate_from_config(lossconfig)
+		self.loss = torch.nn.Identity
+
 		assert ddconfig["double_z"]
+
 		self.quant_conv = torch.nn.Conv2d(2*ddconfig["z_channels"], 2*embed_dim, 1)
 		self.post_quant_conv = torch.nn.Conv2d(embed_dim, ddconfig["z_channels"], 1)
 		self.embed_dim = embed_dim
+
 		if colorize_nlabels is not None:
 			assert type(colorize_nlabels)==int
 			self.register_buffer("colorize", torch.randn(3, colorize_nlabels, 1, 1))
-		if monitor is not None:
-			self.monitor = monitor
-		if ckpt_path is not None:
-			self.init_from_ckpt(ckpt_path, ignore_keys=ignore_keys)
 
-	def init_from_ckpt(self, path, ignore_keys=list()):
-		sd = torch.load(path, map_location="cpu")["state_dict"]
-		keys = list(sd.keys())
-		for k in keys:
-			for ik in ignore_keys:
-				if k.startswith(ik):
-					print("Deleting key {} from state_dict.".format(k))
-					del sd[k]
-		self.load_state_dict(sd, strict=False)
-		print(f"Restored from {path}")
+
 
 	def encode(self, x):
 		h = self.encoder(x)
