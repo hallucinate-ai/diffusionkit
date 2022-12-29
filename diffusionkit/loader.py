a024017eb35c4983c70688df31b3ccb55fd83d48
Mwni <manuel@tigmar.de>
2022-12-29 05:01:01 +0100

    add stable diffusion v2 loader support

--- diffusionkit/loader.py
+++ diffusionkit/loader.py
@@ -4,7 +4,7 @@ from .config import weights
 from .models.diffusion.ddpm import LatentDiffusion
 from .modules.diffusion.openaimodel import UNetModel
 from .models.autoencoder import AutoencoderKL
-from .modules.encoders import FrozenCLIPEmbedder
+from .modules.encoders import FrozenCLIPEmbedder, FrozenOpenCLIPEmbedder
 
 models = dict()
 
@@ -12,8 +12,6 @@ models = dict()
 def load_stable_diffusion(path):
 	if path in models:
 		return models[path]
-
-	checkpoint = torch.load(path, map_location='cpu')
 
 	latent_diffusion_config = {
 		'linear_start': 0.00085,
@@ -54,15 +52,49 @@ def load_stable_diffusion(path):
 		}
 	}
 
-	if checkpoint['state_dict']['model.diffusion_model.input_blocks.0.0.weight'].shape[1] == 9:
-		# checkpoint is for an inpainting model
+	cond_stage_config = {
+		'arch': 'ViT-L-14',
+		'layer': 'last'
+	}
+
+	checkpoint = torch.load(path, map_location='cpu')
+	state_dict = checkpoint['state_dict']
+
+	is_v2_model, is_inpainting_model = guess_stable_diffusion_version(state_dict)
+	is_v_model = is_v2_model and not is_inpainting_model # ugly hack
+
+
+	if is_v2_model:
+		unet_config['num_head_channels'] = 64
+		unet_config['use_linear_in_transformer'] = True
+		unet_config['context_dim'] = 1024
+		unet_config['use_fp16'] = True
+		unet_config['num_heads'] = -1
+		cond_stage = FrozenOpenCLIPEmbedder(
+			pretrained=weights.clip,
+			arch='ViT-H-14',
+			layer='penultimate'
+		)
+	else:
+		cond_stage = FrozenCLIPEmbedder(
+			pretrained=weights.clip
+		)
+
+	if is_v_model:
+		latent_diffusion_config['parameterization'] = 'v'
+
+	if is_inpainting_model:
 		latent_diffusion_config['conditioning_key'] = 'hybrid'
 		unet_config['in_channels'] = 9
 
 
-	unet = UNetModel(**unet_config)
-	first_stage = AutoencoderKL(**first_stage_config)
-	cond_stage = FrozenCLIPEmbedder(weights_file=weights.clip)
+	unet = UNetModel(
+		**unet_config
+	)
+
+	first_stage = AutoencoderKL(
+		**first_stage_config
+	)
 
 	models[path] = model = LatentDiffusion(
 		unet=unet,
@@ -72,13 +104,21 @@ def load_stable_diffusion(path):
 	)
 
 	model.load_state_dict(checkpoint['state_dict'], strict=False)
-	model.is_inpainting_model = model.conditioning_key in ('hybrid', 'concat')
+	model.is_inpainting_model = is_inpainting_model
+	model.is_v_model = is_v_model
+
 	model.half()
 	model.cuda()
 	model.eval()
 
 	return model
 
+
+def guess_stable_diffusion_version(state_dict):
+	is_v2 = state_dict['model.diffusion_model.input_blocks.1.1.transformer_blocks.0.attn2.to_k.weight'].shape[1] == 1024
+	is_inpainting = state_dict['model.diffusion_model.input_blocks.0.0.weight'].shape[1] == 9
+
+	return is_v2, is_inpainting
 
 
 def unload(name):
--- diffusionkit/modules/encoders.py
+++ diffusionkit/modules/encoders.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import open_clip
 from functools import partial
 from transformers import CLIPTokenizer, CLIPTextModel, logging
 from .. import config
@@ -17,11 +18,11 @@ class AbstractEncoder(nn.Module):
 
 
 class FrozenCLIPEmbedder(AbstractEncoder):
-	def __init__(self, weights_file, device='cuda', max_length=77):
+	def __init__(self, pretrained, device='cuda', max_length=77):
 		super().__init__()
 		
-		self.tokenizer = CLIPTokenizer.from_pretrained(weights_file, local_files_only=True)
-		self.transformer = CLIPTextModel.from_pretrained(weights_file, local_files_only=True)
+		self.tokenizer = CLIPTokenizer.from_pretrained(pretrained, local_files_only=True)
+		self.transformer = CLIPTextModel.from_pretrained(pretrained, local_files_only=True)
 		self.device = device
 		self.max_length = max_length
 		self.freeze()
@@ -46,6 +47,76 @@ class FrozenCLIPEmbedder(AbstractEncoder):
 		outputs = self.transformer(input_ids=tokens)
 
 		return outputs.last_hidden_state
+
+	def encode(self, text):
+		return self(text)
+
+
+class FrozenOpenCLIPEmbedder(AbstractEncoder):
+	def __init__(
+		self, 
+		arch, 
+		pretrained, 
+		device='cuda', 
+		max_length=77,
+		layer='last'
+	):
+		super().__init__()
+
+		self.device = device
+		self.max_length = max_length
+		self.model = open_clip.create_model(
+			arch, 
+			device=torch.device('cpu'), 
+			pretrained=pretrained
+		)
+
+		del self.model.visual
+
+		self.freeze()
+
+		if layer == 'last':
+			self.layer_idx = 0
+		elif layer == 'penultimate':
+			self.layer_idx = 1
+		else:
+			raise NotImplementedError()
+
+
+	def freeze(self):
+		self.model = self.model.eval()
+
+		for param in self.parameters():
+			param.requires_grad = False
+
+
+	def forward(self, text):
+		tokens = open_clip.tokenize(text)
+		z = self.encode_with_transformer(tokens.to(self.device))
+
+		return z
+
+
+	def encode_with_transformer(self, text):
+		x = self.model.token_embedding(text)  # [batch_size, n_ctx, d_model]
+		x = x + self.model.positional_embedding
+		x = x.permute(1, 0, 2)  # NLD -> LND
+		x = self.text_transformer_forward(x, attn_mask=self.model.attn_mask)
+		x = x.permute(1, 0, 2)  # LND -> NLD
+		x = self.model.ln_final(x)
+
+		return x
+
+
+	def text_transformer_forward(self, x: torch.Tensor, attn_mask = None):
+		for i, r in enumerate(self.model.transformer.resblocks):
+			if i == len(self.model.transformer.resblocks) - self.layer_idx:
+				break
+
+			x = r(x, attn_mask=attn_mask)
+
+		return x
+
 
 	def encode(self, text):
 		return self(text)
